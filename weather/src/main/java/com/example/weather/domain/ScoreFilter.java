f37acc44360d4d873d6bba50f701e56c9011575f
lmw7414 <lmw7414@naver.com>
2022-05-21 02:39:17 +0900

    amend calScore

--- weather/src/main/java/com/example/weather/domain/ScoreFilter.java
+++ weather/src/main/java/com/example/weather/domain/ScoreFilter.java
@@ -1,5 +1,13 @@
 package com.example.weather.domain;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class ScoreFilter {
 
     private String location;
--- weather/src/main/java/com/example/weather/interfaces/ScoreController.java
+++ weather/src/main/java/com/example/weather/interfaces/ScoreController.java
@@ -1,15 +1,28 @@
 package com.example.weather.interfaces;
 
+import com.example.weather.domain.DailyWeather;
 import com.example.weather.domain.ScoreFilter;
+import com.example.weather.service.WeatherService;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ScoreController {
     @PostMapping("/weathers/score")
-    public ResponseEntity<?> calScore(@RequestBody ScoreFilter resource){
+    public List<Float> calScore(@RequestBody ScoreFilter resource) throws IOException, ParseException {
+
+        WeatherService weatherService = new WeatherService();
+        List<Float> scoreset = new ArrayList<>();
+        List<DailyWeather> dailyWeathers = weatherService.getDailyWeatherData(resource.getLocation());
+
+
         return null;
     }
 }
